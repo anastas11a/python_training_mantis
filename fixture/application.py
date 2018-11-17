8446759b835b5924371e73b7acff9965f6d16577
Anastasiia Churkina <evelevel12@gmail.com>
2018-11-17 19:54:50 +0200

    создание проекта mantis. тест логин

---
+++ fixture/application.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/mantisbt-1.2.20/")
+
+
+    def destroy(self):
+        self.wd.quit()
